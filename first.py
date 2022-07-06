e0c2c2313c911bd874f076b18aba191eeaea4982
Vadim <vadim-perog@mail.ru>
2022-07-06 19:36:57 +0300

    генератор случайных фото котов

--- first.py
+++ first.py
@@ -1,2 +1,8 @@
+import requests
+
+
 print('hello world')
-print ('Good morning'*50)
+print('Good morning'*50)
+response = requests.get("https://api.thecatapi.com/v1/images/search").json()
+random_cat = response[0].get('url')
+print(random_cat)
